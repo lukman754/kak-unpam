bf9fcaf4bcc2bdd6767c7dbcba5d6fa4b8668e65
Lukman Muludin <43158553+lukman754@users.noreply.github.com>
2024-12-20 19:40:03 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,625 @@
+export const kakData = [{
+        "No": 1,
+        "Kegiatan": {
+            "Nama": "Pemakalah dalam pertemuan Ilmiah",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 2,
+        "Kegiatan": {
+            "Nama": "Pemakalah dalam pertemuan Ilmiah",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 3,
+        "Kegiatan": {
+            "Nama": "Pemakalah dalam pertemuan Ilmiah",
+            "Kategori": "Regional/Daerah"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 4,
+        "Kegiatan": {
+            "Nama": "Pemakalah dalam pertemuan Ilmiah",
+            "Kategori": "Internal UNPAM"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 5,
+        "Kegiatan": {
+            "Nama": "Publikasi Ilmiah dalam Jurnal Ilmiah",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 6,
+        "Kegiatan": {
+            "Nama": "Publikasi Ilmiah dalam Jurnal Ilmiah",
+            "Kategori": "Terakreditasi Nasional"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 7,
+        "Kegiatan": {
+            "Nama": "Publikasi Ilmiah dalam Jurnal Ilmiah",
+            "Kategori": "Belum Terakreditasi"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 8,
+        "Kegiatan": {
+            "Nama": "Publikasi Ilmiah dalam Jurnal Ilmiah",
+            "Kategori": "Internal UNPAM"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 9,
+        "Kegiatan": {
+            "Nama": "Menulis Artikel di Media Massa",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 10,
+        "Kegiatan": {
+            "Nama": "Menulis Artikel di Media Massa",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 11,
+        "Kegiatan": {
+            "Nama": "Menulis Artikel di Media Massa",
+            "Kategori": "Regional/Daerah"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 12,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai peserta pada pertemuan ilmiah/Minat Bakat",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 13,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai peserta pada pertemuan ilmiah/Minat Bakat",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 14,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai peserta pada pertemuan ilmiah/Minat Bakat",
+            "Kategori": "Regional/Daerah"
+        },
+        "Nilai": 2
+    },
+    {
+        "No": 15,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengikuti kegiatan kepedulian sosial/Pengabdian Masyarakat",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 16,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengikuti kegiatan kepedulian sosial/Pengabdian Masyarakat",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 17,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengikuti kegiatan kepedulian sosial/Pengabdian Masyarakat",
+            "Kategori": "Regional/Daerah"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 18,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengikuti kegiatan kepedulian sosial/Pengabdian Masyarakat",
+            "Kategori": "Internal UNPAM"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 19,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Prestasi Internasional (Juara I, II, III)"
+        },
+        "Nilai": 30
+    },
+    {
+        "No": 20,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Peserta Kejuaraan Internasional"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 21,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Prestasi Nasional (Juara I, II, III)"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 22,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Peserta Kejuaraan Nasional"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 23,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Prestasi Internal UNPAM (Juara I, II, III)"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 24,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam kompetisi bidang ilmiah/Minat",
+            "Kategori": "Peserta"
+        },
+        "Nilai": 1
+    },
+    {
+        "No": 25,
+        "Kegiatan": {
+            "Nama": "Menjadi Asisten Praktikum/Laboratorium/Mentor",
+            "Kategori": "Koordinator Asisten"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 26,
+        "Kegiatan": {
+            "Nama": "Menjadi Asisten Praktikum/Laboratorium/Mentor",
+            "Kategori": "Asisten"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 27,
+        "Kegiatan": {
+            "Nama": "Menjadi Asisten Praktikum/Laboratorium/Mentor",
+            "Kategori": "Studi Group/Research Group"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 28,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam pameran",
+            "Kategori": "Internasional"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 29,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam pameran",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 30,
+        "Kegiatan": {
+            "Nama": "Ikut serta dalam pameran",
+            "Kategori": "Regional/Daerah"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 31,
+        "Kegiatan": {
+            "Nama": "Proyek Riset",
+            "Kategori": "Proyek Riset UNPAM"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 32,
+        "Kegiatan": {
+            "Nama": "Proyek Riset",
+            "Kategori": "Proyek Riset Fakultas"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 33,
+        "Kegiatan": {
+            "Nama": "Proyek Riset",
+            "Kategori": "Proyek Riset Lembaga"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 34,
+        "Kegiatan": {
+            "Nama": "Proyek Riset",
+            "Kategori": "Proyek Riset Program Studi"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 35,
+        "Kegiatan": {
+            "Nama": "Mahasiswa Bekerja",
+            "Kategori": "Status Sebagai Pegawai Tetap Perusahaan"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 36,
+        "Kegiatan": {
+            "Nama": "Mahasiswa Bekerja",
+            "Kategori": "Wirausaha/Entrepreneur"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 37,
+        "Kegiatan": {
+            "Nama": "Mahasiswa Bekerja",
+            "Kategori": "Part Time (Jam/Minggu)"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 38,
+        "Kegiatan": {
+            "Nama": "Pendaftaran Paten",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 100
+    },
+    {
+        "No": 39,
+        "Kegiatan": {
+            "Nama": "Hak Cipta Buku (Membuat Buku Ber-ISBN)",
+            "Kategori": "Ber-ISBN (buku Asli Diserahkan ke LKA)"
+        },
+        "Nilai": 30
+    },
+    {
+        "No": 40,
+        "Kegiatan": {
+            "Nama": "Karya Cipta Lagu yang Telah Dipublikasikan/Diakui",
+            "Kategori": "Lokal/Wilayah/Nasional"
+        },
+        "Nilai": 30
+    },
+    {
+        "No": 41,
+        "Kegiatan": {
+            "Nama": "Karya Cipta Seni Tari yang Telah Dipentaskan/Didokumentasikan",
+            "Kategori": "Lokal/Wilayah/Nasional"
+        },
+        "Nilai": 30
+    },
+    {
+        "No": 42,
+        "Kegiatan": {
+            "Nama": "Hak Kekayaan Intelektual (HKI)",
+            "Kategori": "Nasional"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 43,
+        "Kegiatan": {
+            "Nama": "Berperan aktif dalam organisasi kemahasiswaan (HIMA/UKM)",
+            "Kategori": "Ketua/Wakil"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 44,
+        "Kegiatan": {
+            "Nama": "Berperan aktif dalam organisasi kemahasiswaan (HIMA/UKM)",
+            "Kategori": "Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 45,
+        "Kegiatan": {
+            "Nama": "Berperan aktif dalam organisasi kemahasiswaan (HIMA/UKM)",
+            "Kategori": "Anggota"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 46,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Universitas Pamulang",
+            "Kategori": "Ketua/Wakil"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 47,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Universitas Pamulang",
+            "Kategori": "Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 48,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Universitas Pamulang",
+            "Kategori": "Anggota"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 49,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Nasional",
+            "Kategori": "Ketua/Wakil"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 50,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Nasional",
+            "Kategori": "Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 51,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Nasional",
+            "Kategori": "Anggota"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 52,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Internasional",
+            "Kategori": "Ketua/Wakil"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 53,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Internasional",
+            "Kategori": "Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 54,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia dalam suatu kegiatan tingkat Internasional",
+            "Kategori": "Anggota"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 55,
+        "Kegiatan": {
+            "Nama": "Berperan serta aktif sebagai peserta pelatihan kepemimpinan",
+            "Kategori": "LKMM (Pra-Dasar, Dasar, Menengah, Lanjutan)"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 56,
+        "Kegiatan": {
+            "Nama": "Berperan serta aktif sebagai peserta pelatihan kepemimpinan",
+            "Kategori": "LDKO"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 57,
+        "Kegiatan": {
+            "Nama": "Berperan serta aktif sebagai peserta",
+            "Kategori": "PKKMB Universitas"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 58,
+        "Kegiatan": {
+            "Nama": "Berperan serta aktif sebagai peserta",
+            "Kategori": "PKKMB Program Studi"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 59,
+        "Kegiatan": {
+            "Nama": "Berperan aktif dalam mempromosikan UNPAM",
+            "Kategori": "Kegiatan Promosi"
+        },
+        "Nilai": 7
+    },
+    {
+        "No": 60,
+        "Kegiatan": {
+            "Nama": "Upacara Bendera",
+            "Kategori": "Petugas"
+        },
+        "Nilai": 4
+    },
+    {
+        "No": 61,
+        "Kegiatan": {
+            "Nama": "Upacara Bendera",
+            "Kategori": "Peserta"
+        },
+        "Nilai": 2
+    },
+    {
+        "No": 62,
+        "Kegiatan": {
+            "Nama": "Magang Internal UNPAM",
+            "Kategori": "Pengembang Sistem"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 63,
+        "Kegiatan": {
+            "Nama": "Magang Internal UNPAM",
+            "Kategori": "Tim Magang"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 64,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengusulkan proposal dalam kompetisi yang diselenggarakan oleh kemdikbudristek",
+            "Kategori": "Sebagai Ketua"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 65,
+        "Kegiatan": {
+            "Nama": "Berperan aktif mengusulkan proposal dalam kompetisi yang diselenggarakan oleh kemdikbudristek",
+            "Kategori": "Sebagai Anggota"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 66,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai tim operator tracer study",
+            "Kategori": "Sebagai Ketua"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 67,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai tim operator tracer study",
+            "Kategori": "Sebagai Anggota"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 68,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia penyelenggara kejuaraan tingkat Nasional",
+            "Kategori": "Sebagai Ketua"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 69,
+        "Kegiatan": {
+            "Nama": "Berperan aktif sebagai panitia penyelenggara kejuaraan tingkat Nasional",
+            "Kategori": "Sebagai Anggota"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 70,
+        "Kegiatan": {
+            "Nama": "Juri/Pelatih",
+            "Kategori": "Regional/Nasional/Internasional"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 71,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Internasional",
+            "Kategori": "Ketua/Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 25
+    },
+    {
+        "No": 72,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Internasional",
+            "Kategori": "Anggota Biasa"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 73,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Nasional/Provinsi",
+            "Kategori": "Ketua/Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 20
+    },
+    {
+        "No": 74,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Nasional/Provinsi",
+            "Kategori": "Anggota Biasa"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 75,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Kabupaten/Kecamatan",
+            "Kategori": "Ketua/Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 15
+    },
+    {
+        "No": 76,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat Kabupaten/Kecamatan",
+            "Kategori": "Anggota Biasa"
+        },
+        "Nilai": 5
+    },
+    {
+        "No": 77,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat RT/RW",
+            "Kategori": "Ketua/Sekretaris/Bendahara/Kabid"
+        },
+        "Nilai": 10
+    },
+    {
+        "No": 78,
+        "Kegiatan": {
+            "Nama": "Organisasi Ekstra Kampus - Organisasi Tingkat RT/RW",
+            "Kategori": "Anggota Biasa"
+        },
+        "Nilai": 5
+    }
+];
